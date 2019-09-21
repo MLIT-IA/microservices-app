ef0bdda67ed1bb386b2119be131c4f65b9ef9cee
machinelearnit <mehdi.dellagi@machinelearnit.com>
2019-09-21 20:29:35 +0200

    Client: UserStatus and NavBar Management (LogIn/Logout display)

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout'
+import UserStatus from './components/UserStatus'
 
 class App extends Component {
   constructor() {
@@ -106,7 +107,10 @@ class App extends Component {
   render() {
       return (
         <div>
-          <NavBar title={this.state.title} />
+          <NavBar
+            title={this.state.title}
+            isAuthenticated={this.state.isAuthenticated}
+           />
           <section className="section">
             <div className="container">
               <div className="columns">
@@ -152,6 +156,7 @@ class App extends Component {
                         isAuthenticated={this.state.isAuthenticated}
                       />
                     )} />
+                    <Route exact path='/status' component={UserStatus}/>
                   </Switch>
                 </div>
               </div>
